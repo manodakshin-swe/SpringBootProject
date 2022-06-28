eec6d676c62a3c48366722c439a3ca1e8cc2760a
manodakshin-swe <107427286+manodakshin-swe@users.noreply.github.com>
2022-06-28 15:06:40 -0400

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+//Importing required classes
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//Annotation
+@Controller
+
+//Class
+public class DemoController {
+
+ @RequestMapping("/hello")
+ @ResponseBody
+
+ // Method
+ public String helloWorld()
+ {
+     // Print statement
+     return "Goodbye everyone!";
+ }
+}
